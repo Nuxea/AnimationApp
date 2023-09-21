67f823c662f2761790740de9c2898f4dccd462b0
amandine lescieux (ext) <amandine.lescieux@FR-25B2ML3.groupinfra.com>
2023-09-21 10:37:45 +0200

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import { ref } from "vue";
+
+const showGreet = ref(true)
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <Transition name="fade" >
+        <h1 v-if="showGreet" >Hello World !</h1>
+        <h1 v-else>Bye World !</h1>
+      </Transition>
+    </div>
+
+    <button @click="showGreet = !showGreet">Toggle</button>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+}
+
+h1 {
+  position: absolute;
+}
+
+button {
+  margin-top: 50px;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-enter-active {
+  transition: all 0.5s ease;
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+import {ref} from "vue";
+
+const names = ref(["Selena Damo","Kevin Johnstone"])
+const nameInput = ref("")
+
+const addInvitee = () => {
+  names.value.unshift(nameInput.value)
+  nameInput.value = ""
+}
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <input v-model="nameInput" @keydown.enter="addInvitee" type="text" name="" id="" placeholder="Add person to invite...">
+      <ul>
+        <li v-for="name in names" :key="name">
+          {{ name }}
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  max-width: 300px;
+  margin: 0 auto;
+}
+
+.container input {
+  width: 100%;
+  border-radius: 5px;
+  border: 1px solid rgba(128,128,128,0.13);
+  padding: 10px;
+  margin: 20px auto;
+  box-shadow: 1px 1px 10px rgba(0,0,0,1);
+}
+
+ul {
+  display: inline;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  width: 300px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-top: 10px;
+  background-color: #3b3b3b;
+  box-shadow: 1px 1px 10px rgba(0,0,0,1);
+  text-align: center;
+  cursor: pointer;
+}
+</style>
